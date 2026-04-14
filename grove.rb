ef1017178e300ea791b64857ce7985d929d1474a
yaomy <yaomy@motern.com>
2026-04-14 10:59:47 +0800

    chore: fix sha256 checksums for v0.2.0

--- grove.rb
+++ grove.rb
@@ -7,16 +7,16 @@ class Grove < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/yauyy/grove/releases/download/v#{version}/grove-aarch64-apple-darwin.tar.gz"
-      sha256 "5afdddf31d6bafe93acf819bef130495b06a895b3313188e9f691b6f81a1c791"
+      sha256 "513537c0249c5ed511d6541842c19f4ffe29f84a01e973e216cea91efea6efb2"
     else
       url "https://github.com/yauyy/grove/releases/download/v#{version}/grove-x86_64-apple-darwin.tar.gz"
-      sha256 "6055b4758dd9d3fda96717fc99dfa499903309e4c6201130e4352588f8946f57"
+      sha256 "741c8cfcc56cfc1cfc55817151ab8511ba1b61a5c1fcbf18db713781396dc32b"
     end
   end
 
   on_linux do
     url "https://github.com/yauyy/grove/releases/download/v#{version}/grove-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "5b919e9341984888b234992e367c286c1bd5807ecba36d5988bf8357d1128215"
+    sha256 "1d625c5545854939ae761314e4bc936585e2818042ddabca6af156d5c7b70e07"
   end
 
   def install
